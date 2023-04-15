9f6f12da0a8e0fcc05ed19e112983cad07d4cfc4
Gabriele31201 <gabri31201@gmail.com>
2023-04-15 17:34:19 +0200

    add lil design

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-﻿string[] favoriteFood = { "Pizza", "Carne", "Pasta", "Cioccolato", "Sushi", };
+﻿
+
+
+
+Console.WriteLine("------------------------- IN QUESTA CLASSIFICA TROVIAMO 5 POSIZIONI --------------------------------");
+
+
+
+string[] favoriteFood = { "Pizza", "Carne", "Pasta", "Cioccolato", "Sushi", };
 
 for (int i = 0; i < favoriteFood.Length; i++)
 {
     Console.WriteLine("-" + (i + 1) + "° " + favoriteFood[i]);
 }
+
+Console.WriteLine("----------------------------------------------------------------------------------------------------");
