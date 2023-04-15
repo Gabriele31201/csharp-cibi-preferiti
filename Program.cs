311ae7321a70ed350ec44ddff1e826bc6a8729f6
Gabriele31201 <gabri31201@gmail.com>
2023-04-15 17:26:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] favoriteFood = { "Pizza", "Carne", "Pasta", "Cioccolato", "Sushi", };
+
+for (int i = 0; i < favoriteFood.Length; i++)
+{
+    Console.WriteLine("-" + (i + 1) + "° " + favoriteFood[i]);
+}
